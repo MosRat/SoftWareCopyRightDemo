9d24d5392bc9ec76cd337aa25d16045921b14f34
文泓力 <108924227+MosRat@users.noreply.github.com>
2023-07-26 13:51:19 +0800

    加入评价、教学与反馈页面

--- FocusInterFace.py
+++ FocusInterFace.py
@@ -1,11 +1,90 @@
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QPixmap, QPainter, QColor
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QGraphicsDropShadowEffect
+from qfluentwidgets import FluentIcon as FIF
 
 from demo1 import Ui_Form
+from teach import Ui_teach
+from upload import Ui_Upload
+from evals import Ui_Eval
 
 
 class Page(QWidget, Ui_Form):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
+        # self.setImage("PyQt-Fluent-Widgets/examples/acrylic_label/resource/埃罗芒阿老师.jpg")
         self.setupUi(self)
+        self.setShadowEffect(self.CardWidget)
+        self.setShadowEffect(self.CardWidget_2)
+
+    def setShadowEffect(self, card: QWidget):
+        shadowEffect = QGraphicsDropShadowEffect(self)
+        shadowEffect.setColor(QColor(0, 0, 0, 15))
+        shadowEffect.setBlurRadius(10)
+        shadowEffect.setOffset(0, 0)
+        card.setGraphicsEffect(shadowEffect)
+
+
+class Page1(QWidget, Ui_teach):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        # self.setImage("PyQt-Fluent-Widgets/examples/acrylic_label/resource/埃罗芒阿老师.jpg")
+        self.setupUi(self)
+        # self.setShadowEffect(self.CardWidget)
+        # self.setShadowEffect(self.CardWidget_2)
+
+    def setShadowEffect(self, card: QWidget):
+        shadowEffect = QGraphicsDropShadowEffect(self)
+        shadowEffect.setColor(QColor(0, 0, 0, 15))
+        shadowEffect.setBlurRadius(10)
+        shadowEffect.setOffset(0, 0)
+        card.setGraphicsEffect(shadowEffect)
+
+
+class Page2(QWidget, Ui_Upload):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        # self.setImage("PyQt-Fluent-Widgets/examples/acrylic_label/resource/埃罗芒阿老师.jpg")
+        self.setupUi(self)
+        self.setShadowEffect(self.CardWidget)
+        self.setShadowEffect(self.CardWidget_2)
+        self.PixmapLabel.setPixmap(QPixmap("./src/img/teacher.png"))
+
+    def setShadowEffect(self, card: QWidget):
+        shadowEffect = QGraphicsDropShadowEffect(self)
+        shadowEffect.setColor(QColor(0, 0, 0, 15))
+        shadowEffect.setBlurRadius(10)
+        shadowEffect.setOffset(0, 0)
+        card.setGraphicsEffect(shadowEffect)
+
+
+class Page3(QWidget, Ui_Eval):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        # self.setImage("PyQt-Fluent-Widgets/examples/acrylic_label/resource/埃罗芒阿老师.jpg")
+        self.setupUi(self)
+        self.IconWidget.setIcon(FIF.EXPRESSIVE_INPUT_ENTRY)
+        self.IconWidget_2.setIcon(FIF.UPDATE)
+        self.IconWidget_3.setIcon(FIF.GLOBE)
+        self.PixmapLabel_3.setPixmap(
+            QPixmap('./src/img/human.jpg').scaled(self.PixmapLabel.width(), self.PixmapLabel.height()))
+        self.PixmapLabel_4.setPixmap(
+            QPixmap('./src/img/human_dte.jpg').scaled(self.PixmapLabel_2.width(), self.PixmapLabel_2.height()))
+        self.PixmapLabel.setPixmap(
+            QPixmap('./src/img/curl.png').scaled(self.PixmapLabel_3.width(), self.PixmapLabel_3.height()))
+        self.PixmapLabel_2.setPixmap(
+            QPixmap('./src/img/curl2.png').scaled(self.PixmapLabel_4.width(), self.PixmapLabel_4.height()))
+
+        # self.setShadowEffect(self.CardWidget)
+        # self.setShadowEffect(self.CardWidget_2)
+
+    def setShadowEffect(self, card: QWidget):
+        shadowEffect = QGraphicsDropShadowEffect(self)
+        shadowEffect.setColor(QColor(0, 0, 0, 15))
+        shadowEffect.setBlurRadius(10)
+        shadowEffect.setOffset(0, 0)
+        card.setGraphicsEffect(shadowEffect)
+# class SubPage1(QWidget, Ui_Form):
+#     def __init__(self, parent=None):
+#         super().__init__(parent=parent)
+#         self.setupUi(self)
--- main.py
+++ main.py
@@ -4,20 +4,33 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QFont, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from qfluentwidgets import SplitFluentWindow, FluentIcon, NavigationAvatarWidget, NavigationItemPosition, setTheme, \
-    Theme,setThemeColor,ThemeColor
+    Theme, setThemeColor, ThemeColor
+from qfluentwidgets.components.widgets.acrylic_label import AcrylicLabel
 
-from FocusInterFace import Page
+from FocusInterFace import Page, Page1, Page2, Page3
 
 
 class MainInterFace(SplitFluentWindow):
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Demo')
-        self.setWindowIcon(QIcon('./src/img/icon.png'))
+        # self.setWindowTitle('Demo')
+        # self.setWindowIcon(QIcon('./src/img/icon.png'))
+        # self.setGeometry(50, 50, 1600, 800)
 
         self.focusInterFace = Page(self)
-        self.addSubInterface(self.focusInterFace, FluentIcon.DOWNLOAD, 'xy')
+        self.focusInterFace1 = Page1(self)
+        self.focusInterFace2 = Page2(self)
+        self.focusInterFace3 = Page3(self)
+        # self.w = AcrylicLabel(20, QColor(105, 114, 168, 102), parent=self)
+        # self.focusInterFace1 = SubPage1(self)
+        self.addSubInterface(self.focusInterFace, FluentIcon.HOME, 'xy')
+        self.addSubInterface(self.focusInterFace1, FluentIcon.TAG, 'xyz')
+        self.addSubInterface(self.focusInterFace2, FluentIcon.UPDATE, 'xy1z')
+        self.addSubInterface(self.focusInterFace3, FluentIcon.TRAIN, 'xy1z1')
+        # self.addSubInterface(self.focusInterFace1, FluentIcon.ADD, 'xy1')
+        # self.addSubInterface(self.focusInterFace1, FluentIcon.HOME, 'xy2')
+        # self.addSubInterface(self.w, FluentIcon.CAFE, 'xy1')
 
         self.navigationInterface.addWidget(
             routeKey='avatar',
@@ -31,6 +44,18 @@ class MainInterFace(SplitFluentWindow):
             text='设置',
             position=NavigationItemPosition.BOTTOM
         )
+
+        self.navigationInterface.setExpandWidth(280)
+        self.initWindow()
+
+    def initWindow(self):
+        self.resize(1200, 800)
+        self.setWindowIcon(QIcon('./src/img/logo.png'))
+        self.setWindowTitle('Demo')
+
+        desktop = QApplication.desktop().availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w // 2 - self.width() // 2, h // 2 - self.height() // 2)
 
 
 if __name__ == '__main__':
